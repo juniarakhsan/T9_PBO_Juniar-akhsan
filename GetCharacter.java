d1658c139033ece1220ac271d4c8387e4aad62e2
juniarakhsan <124061925+juniarakhsan@users.noreply.github.com>
2024-05-11 23:35:13 +0700

    Create GetCharacter.java

---
+++ GetCharacter.java
@@ -0,0 +1,8 @@
+public class GetCharacter {
+    public static void main(String[] args) {
+        String var1 = "Belajar Java di Duniailkom";
+        System.out.println(var1.charAt(0)); // prints "B"
+        System.out.println(var1.charAt(1)); // prints "e"
+        // ...
+    }
+}
