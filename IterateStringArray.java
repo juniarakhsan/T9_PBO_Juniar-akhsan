296a674aec6578702ed041aa3a0aefa113b5a9eb
juniarakhsan <124061925+juniarakhsan@users.noreply.github.com>
2024-05-11 23:36:35 +0700

    Create IterateStringArray.java

---
+++ IterateStringArray.java
@@ -0,0 +1,9 @@
+public class IterateStringArray {
+    public static void main(String[] args) {
+        String[] stringArray = {"violin", "viola", "cello", "double bass"};
+        for (String s : stringArray) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+}
