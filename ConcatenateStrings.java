9501d25fc2af74f8f7754efe9acf16faba4a96e9
juniarakhsan <124061925+juniarakhsan@users.noreply.github.com>
2024-05-11 23:34:29 +0700

    Create ConcatenateStrings.java

---
+++ ConcatenateStrings.java
@@ -0,0 +1,7 @@
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+        String var1 = "Belajar Java";
+        String var2 = " di Duniailkom";
+        System.out.print(var1 + var2);
+    }
+}
